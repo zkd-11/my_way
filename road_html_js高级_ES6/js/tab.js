5d4f1978caa81776ec5f6598e565089d991ab03b
zkd-11 <1137756821@qq.com>
2022-11-09 20:40:35 +0800

    updpwn

---
+++ road_html_js高级_ES6/js/tab.js
@@ -0,0 +1,12 @@
+class Tab{
+    constructor(){}
+    // 1. 切换功能
+    toggleTab(){}
+    // 2. 添加功能
+    addTab(){}
+    // 3. 删除功能
+    removTab(){}
+    // 4. 修改功能
+    editTab(){} 
+}
+    new Tab();
